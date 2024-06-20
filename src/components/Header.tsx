618afef78859c113f9bea28ea776d221cadbdb06
francisco-solis99 <franciscosoliscss@gmail.com>
2024-06-20 17:01:28 -0600

    [chore] make header ocmponent

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className='app__header'>
+      <h1 className='app__title'>
+        <span>Is It Fill?</span>
+        <span>Is It Fill?</span>
+      </h1>
+    </header>
+  )
+}
